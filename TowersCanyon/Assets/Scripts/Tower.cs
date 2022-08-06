03aaa4d21d5117a8b76a5a205a3a5ecf6b3b91d5
antronik7 <antoineberthiaumedutil@gmail.com>
2022-08-05 23:59:17 -0400

    Projectile
    -Added a black material.
    -Added a projectile prefab.
    -Added property to make tower spawn projectiles.

--- TowersCanyon/Assets/Scripts/Tower.cs
+++ TowersCanyon/Assets/Scripts/Tower.cs
@@ -1,11 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField]
+    private float attackSpeed;
+    [SerializeField]
+    private float attackRange;
+    [SerializeField]
+    private float projectileSpeed;
+
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private Transform projectileSpawnPosition;
+    [SerializeField]
     private Renderer towerRenderer;
+
+    private SphereCollider attackRangeCollider;
+
+    private float attackSpeedTimer = 0;
+    private List<Enemy> enemyInRange = new List<Enemy>();
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        attackRangeCollider = GetComponent<SphereCollider>();
+        SetAttackRange();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +38,40 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageAttack();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy potentialEnemy = other.GetComponent<Enemy>();
+        if (potentialEnemy)
+            enemyInRange.Add(potentialEnemy);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Enemy potentialEnemy = other.GetComponent<Enemy>();
+        if (potentialEnemy)
+            enemyInRange.Remove(potentialEnemy);
+    }
+
+    private void ManageAttack()
+    {
+        attackSpeedTimer -= Time.deltaTime;
+
+        if (attackSpeedTimer <= 0f)
+        {
+            if (enemyInRange.Count > 0)
+                SpawnProjectile();
+            else
+                attackSpeedTimer = 0f;
+        }
+    }
+
+    private void SpawnProjectile()
+    {
+        GameObject newProectile = Instantiate(projectile, projectileSpawnPosition.position, Quaternion.identity) as GameObject;
+        newProectile.GetComponent<Projectile>().Launch(enemyInRange[0].transform, projectileSpeed);
     }
 
     public void MakeTransparent(bool transparent)
@@ -28,5 +83,10 @@ public class Tower : MonoBehaviour
             color.a = 1f;
 
         towerRenderer.material.color = color;
+    }
+
+    private void SetAttackRange()
+    {
+        attackRangeCollider.radius = attackRange;
     }
 }
